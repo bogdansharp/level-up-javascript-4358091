abfdbd40e6fbcf11ac997385233efc7e5d9c2ebc
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-25 21:49:14 +0000

    v2

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,13 +1,17 @@
 // Write your code here
+
 class Book {
-  constructor(title, author, isbn) {
+  constructor(title, author, isbn, numCopies = 0) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
-    this.numCopies = 0;
+    this.numCopies = numCopies;
   }
-  get getAvailability() {
-    return (!this.numCopies) ? "out of stock" : 
+  get availability() { 
+    return this.getAvailability();
+  }
+  getAvailability() {
+    return (! this.numCopies) ? "out of stock" : 
         (this.numCopies < 10) ? "low stock" : "in stock";
   }
   sell(numSold = 1) {
@@ -18,3 +22,53 @@ class Book {
     this.numCopies += numRestock;
   }
 }
+
+
+// function Book(title, author, isbn, numCopies = 0) {
+//   return {
+//     title : title,
+//     author : author,
+//     isbn : isbn,
+//     numCopies : numCopies,
+//     getAvailability : function() {
+//       return (!this.numCopies) ? "out of stock" : 
+//         (this.numCopies < 10) ? "low stock" : "in stock";
+//     },
+//     sell : function(numSold = 1) {
+//       this.numCopies -= numSold;
+//       if (this.numCopies < 0) this.numCopies = 0;
+//     },
+//     restock : function(numRestock = 5) {
+//       this.numCopies += numRestock;
+//     }
+//   }
+// }
+
+
+// function Book(title, author, isbn, numCopies = 0) {
+//   this.title = title;
+//   this.author = author;
+//   this.isbn = isbn;
+//   this.numCopies = 0;
+// }
+// Book.prototype.getAvailability = () => {
+//   return (!this.numCopies) ? "out of stock" : 
+//     (this.numCopies < 10) ? "low stock" : "in stock";
+// }   
+// Book.prototype.sell = (numSold = 1) => {
+//   this.numCopies -= numSold;
+//   if (this.numCopies < 0) this.numCopies = 0;
+// }
+// Book.prototype.restock = (numRestock = 5) => {
+//   this.numCopies += numRestock;
+// }
+
+// test
+var b1 = new Book("JS for culchies", "Dostoevsky", "666", 12);
+console.log(b1.getAvailability())
+b1.sell(10);
+console.log(b1.getAvailability())
+b1.sell(10);
+console.log(b1.availability)
+b1.restock(666);
+console.log(b1.availability)

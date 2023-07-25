4015c5b0d32648ff79f1f0e1a2cec9c1f03f97b9
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-25 21:21:22 +0000

    v1

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,2 +1,20 @@
 // Write your code here
-//test
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = 0;
+  }
+  get getAvailability() {
+    return (!this.numCopies) ? "out of stock" : 
+        (this.numCopies < 10) ? "low stock" : "in stock";
+  }
+  sell(numSold = 1) {
+    this.numCopies -= numSold;
+    if (this.numCopies < 0) this.numCopies = 0;
+  }
+  restock(numRestock = 5) {
+    this.numCopies += numRestock;
+  }
+}

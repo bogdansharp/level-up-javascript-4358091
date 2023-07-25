354e3f8491cbb38676d534ec5a326646aadd1220
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-25 22:14:53 +0000

    01_02_movie_prototype

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+function Movie(title, director, genre, year, rating = 0) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.year = year;
+  this.rating = rating;
+}
+Movie.prototype.getOverview = function() { 
+  return `${this.title}, a ${this.genre} film ` +
+    `directed by ${this.director} ` +
+    `was released in ${this.year} year. ` +
+    `It recieved a rating of ${this.rating}`;
+}
+// test
+var b1 = new Movie("JS for culchies", "Dostoevsky", "scary", 2023);
+console.log(b1.getOverview())
+console.log(b1)

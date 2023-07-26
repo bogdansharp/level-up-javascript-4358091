c31f2e2a080c35ef247f5f04834f871d7cd2f539
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-26 10:15:00 +0000

    01_07_classroom_attendance

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,15 @@
 // Write your code here
+const getStudents = (param) => {
+  let {hasTeachingAssistant, classList} = param;
+  if (hasTeachingAssistant)
+    [teacher, assistant, ...students] = classList;
+  else
+    [teacher, ...students] = classList;
+  return students;
+}
+
+// test
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}));

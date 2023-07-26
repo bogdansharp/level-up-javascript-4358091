6d530ac188cefef35d23a49339c9b92167767445
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-26 09:40:02 +0000

    01_05_private_object

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,21 @@
 // Write your code here
+
+const _privateUserName = Symbol('userName');
+const _privatePassword = Symbol('password');
+class User {
+  constructor(userName, password, age) {
+    this['age'] = age;
+    this[_privateUserName] = userName;
+    this[_privatePassword] = password;
+  }
+}
+
+// test
+var u1 = new User('Vasia', 'god', 77);
+var u2 = new User('Petya', '123', 7);
+console.log(u1);
+console.log(u1.userName);
+console.log(u1.password);
+console.log(u2);
+console.log(u2.userName);
+console.log(u2.password);

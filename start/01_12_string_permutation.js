ca8dc31d2ed2235f7c5ca3c89225f4f9e6c14e86
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-26 23:57:58 +0000

    01_12_string_permutation

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,6 @@
 // Write your code here
+const factorial = (n) => n < 2 ? 1 : n * factorial(n - 1);
+
+// test
+console.log(factorial("Hello world".length));
+console.log(factorial("Scrabble".length));

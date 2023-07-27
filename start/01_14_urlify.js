d4f2db65b2f421ebc53c9bcb75e8af3cfe910e39
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-27 13:21:10 +0000

    01_14_urlify v2

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,10 +1,13 @@
 // Write your code here
 
 String.prototype.urlfy = function() {
-  return this.replace(/[^a-zA-Z\s]/g, '').replace(/\s+/g, '-')
+  return this.trim()
+    .replace(/[^a-zA-Z\s]/g, '')
+    .replace(/\s+/g, '-')
     .toLocaleLowerCase();
 }
 
 //test
 console.log("My Blog   Name!".urlfy());
 console.log("Emma's Blog".urlfy());
+console.log("How I Got Into Programming!!!".urlfy());

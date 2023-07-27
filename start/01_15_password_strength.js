803af703483bb2f082825901c5ddf49a2b04db6e
bogdansharp <132409339+bogdansharp@users.noreply.github.com>
2023-07-27 13:44:25 +0000

    01_15_password_strength

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,12 @@
 // Write your code here
+const passwordStrength = pass =>
+  pass.length > 7 && 
+  /^.*[a-z].*$/g.test(pass) && 
+  /^.*[A-Z].*$/g.test(pass) && 
+  /^.*\d.*$/g.test(pass) &&
+  /^.*[!@#$%^&*].*$/g.test(pass) ?
+  "Your password is valid" : "Your password is invalid"
+  
+  // test
+console.log(passwordStrength("abc"));
+console.log(passwordStrength("9Ab!4567"));
